bbe2398a23e8bc311cd48e4e21ecde05e4acefa5
nicolaibye <nicbye02217@stud.noroff.no>
2025-11-12 15:53:43 +0100

    Added typescript and cleaned up project a bit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+import { root } from ".";
+
+function App() {
+  return <div>hello world</div>;
+}
+
+export default App;
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
