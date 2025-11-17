ba81d636da27c5d00b8f1f23407af443fd3a5ab8
nicolaibye <nicbye02217@stud.noroff.no>
2025-11-17 17:29:11 +0100

    Creating a simple setup since it is not required to be functional for final outcome

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,20 @@
+import NewsCard from "../../comp/NewsCard";
+
 function Home() {
   return (
-    <div>
+    <div className="flex gap-5 flex-col">
       <h1 className="absolute w-0.5 h-0.5 overflow-hidden p-0 -m-0.5 whitespace-nowrap border-0 top-0 left-0">
         Holidaze Accommodation homepage
       </h1>
+      <div className="flex flex-col md:flex-row gap-5">
+        <NewsCard />
+        <div className="hidden md:block">
+          <NewsCard />
+        </div>
+        <div className="hidden md:block">
+          <NewsCard />
+        </div>
+      </div>
     </div>
   );
 }
