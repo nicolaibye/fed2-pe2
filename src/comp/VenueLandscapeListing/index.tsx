e985abc6c6a90bc39d6d787574ae69ba2f6ec782
nicolaibye <nicbye02217@stud.noroff.no>
2025-11-19 10:50:27 +0100

    Added basic setup for swapping between adventure showcases

--- src/comp/VenueLandscapeListing/index.tsx
+++ src/comp/VenueLandscapeListing/index.tsx
@@ -67,32 +67,78 @@
 //   };
 // }
 
+import { useAdventureContext } from "../../context/AdventureContext/useAdventureContext";
+
 function VenueLandscapeListing() {
   // const { id, name, description, media, price, rating } = data;
+  const { adventureType } = useAdventureContext();
   return (
-    <li className={`max-w-[90%] h-fit rounded-sm flex flex-col`}>
+    <li
+      className={`w-[90%] h-52 rounded-sm flex ${adventureType === "affluent" ? "flex-col" : "flex-row"} `}
+    >
       <img
         src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSR3p565lkyXBLlkNemqfGmUAWtVxN5XT6cQg&s"
         alt=""
-        className="w-full max-h-[55%] object-cover"
+        className={`flex-1 object-cover ${adventureType === "affluent" ? "w-full min-h-0" : "h-full min-w-2"} `}
       />
       <div className="flex flex-row">
-        <div className="font-sans w-60 bg-hdYellow p-2 flex flex-col gap-2">
-          <div>
-            <h3 className="font-sans font-bold leading-5">
-              Clarion Hotel The Hub
-            </h3>
-            <p>Milan, Italy</p>
-          </div>
-          <div className="flex flex-row items-center gap-2 text-sm">
-            <p>4.77</p>
-            <div className="bg-hdBlack w-0.5 h-0.5 rounded-full"></div>
-            <p>
-              <span className="font-bold">£500</span> for 4 nights
-            </p>
-          </div>
+        <div
+          className={`font-sans w-60 bg-hdYellow p-2 flex flex-col gap-2 ${adventureType === "affluent" ? "" : "justify-between"}`}
+        >
+          {adventureType === "affluent" ? (
+            <>
+              <div>
+                <h3 className="font-sans font-bold leading-5">
+                  Clarion Hotel The Hub
+                </h3>
+                <p>Milan, Italy</p>
+              </div>
+              <div className="flex flex-row items-center gap-2 text-sm">
+                <p className="font-bold">4.77</p>
+                <div className="bg-hdBlack w-0.5 h-0.5 rounded-full"></div>
+                <p>
+                  <span className="font-bold">£500</span> for 4 nights
+                </p>
+              </div>
+            </>
+          ) : (
+            <>
+              <div>
+                <h3 className="font-sans font-bold leading-5">
+                  Clarion Hotel The Hub
+                </h3>
+                <p>Milan, Italy</p>
+                <div className="flex flex-row items-center gap-2 text-sm">
+                  <p className="font-bold">4.77</p>
+                  <div className="bg-hdBlack w-0.5 h-0.5 rounded-full"></div>
+                  <p>200 reviews</p>
+                </div>
+                <ul
+                  className={`flex flex-col text-hdRed text-sm ${adventureType === "explorer" ? "block" : "hidden"} `}
+                >
+                  <li>Wifi</li>
+                  <li>Parking</li>
+                  <li>Breakfast</li>
+                  <li>Pets allowed</li>
+                </ul>
+              </div>
+              <div>
+                <ul
+                  className={`flex flex-col text-hdRed text-sm ${adventureType === "guardian" ? "block" : "hidden"} `}
+                >
+                  <li>Free cancellation</li>
+                  <li>No prepayment needed</li>
+                </ul>
+                <p className="text-lg">
+                  <span className="font-bold">£125</span> for 4 nights
+                </p>
+              </div>
+            </>
+          )}
         </div>
-        <button className="bg-hdRed font-light text-hdWhite leading-5">
+        <button
+          className={`bg-hdRed font-light text-hdWhite leading-5 ${adventureType === "affluent" ? "block" : "hidden"} `}
+        >
           Express Booking
         </button>
       </div>
