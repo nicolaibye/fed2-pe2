3a091a7a5d81c0685b673016d15bdc0f034d9312
nicolaibye <nicbye02217@stud.noroff.no>
2025-11-20 13:13:40 +0100

    Started building out search with filter section for setup

---
+++ src/comp/FilterButton/index.tsx
@@ -0,0 +1,128 @@
+function FilterButton() {
+  return (
+    <>
+      <form
+        action=""
+        className="flex flex-row flex-wrap justify-center gap-4 min-w-0"
+      >
+        <div className="flex flex-col items-start font-sans">
+          <h3 className="font-bold text-base mb-1">Price per night</h3>
+          <div className="flex flex-row-reverse gap-2">
+            <label htmlFor="">Under €50</label>
+            <input type="checkbox" />
+          </div>
+          <div className="flex flex-row-reverse gap-2">
+            <label htmlFor="">€50-100</label>
+            <input type="checkbox" />
+          </div>
+          <div className="flex flex-row-reverse gap-2">
+            <label htmlFor="">€100-200</label>
+            <input type="checkbox" />
+          </div>
+          <div className="flex flex-row-reverse gap-2">
+            <label htmlFor="">€200+</label>
+            <input type="checkbox" />
+          </div>
+        </div>
+        <div className="flex flex-col items-start font-sans">
+          <h3 className="font-bold text-base mb-1">Price per night</h3>
+          <div className="flex flex-row-reverse gap-2">
+            <label htmlFor="">Under €50</label>
+            <input type="checkbox" />
+          </div>
+          <div className="flex flex-row-reverse gap-2">
+            <label htmlFor="">€50-100</label>
+            <input type="checkbox" />
+          </div>
+          <div className="flex flex-row-reverse gap-2">
+            <label htmlFor="">€100-200</label>
+            <input type="checkbox" />
+          </div>
+          <div className="flex flex-row-reverse gap-2">
+            <label htmlFor="">€200+</label>
+            <input type="checkbox" />
+          </div>
+        </div>
+        <div className="flex flex-col items-start font-sans">
+          <h3 className="font-bold text-base mb-1">Price per night</h3>
+          <div className="flex flex-row-reverse gap-2">
+            <label htmlFor="">Under €50</label>
+            <input type="checkbox" />
+          </div>
+          <div className="flex flex-row-reverse gap-2">
+            <label htmlFor="">€50-100</label>
+            <input type="checkbox" />
+          </div>
+          <div className="flex flex-row-reverse gap-2">
+            <label htmlFor="">€100-200</label>
+            <input type="checkbox" />
+          </div>
+          <div className="flex flex-row-reverse gap-2">
+            <label htmlFor="">€200+</label>
+            <input type="checkbox" />
+          </div>
+        </div>
+        <div className="flex flex-col items-start font-sans">
+          <h3 className="font-bold text-base mb-1">Price per night</h3>
+          <div className="flex flex-row-reverse gap-2">
+            <label htmlFor="">Under €50</label>
+            <input type="checkbox" />
+          </div>
+          <div className="flex flex-row-reverse gap-2">
+            <label htmlFor="">€50-100</label>
+            <input type="checkbox" />
+          </div>
+          <div className="flex flex-row-reverse gap-2">
+            <label htmlFor="">€100-200</label>
+            <input type="checkbox" />
+          </div>
+          <div className="flex flex-row-reverse gap-2">
+            <label htmlFor="">€200+</label>
+            <input type="checkbox" />
+          </div>
+        </div>
+        <div className="flex flex-col items-start font-sans">
+          <h3 className="font-bold text-base mb-1">Price per night</h3>
+          <div className="flex flex-row-reverse gap-2">
+            <label htmlFor="">Under €50</label>
+            <input type="checkbox" />
+          </div>
+          <div className="flex flex-row-reverse gap-2">
+            <label htmlFor="">€50-100</label>
+            <input type="checkbox" />
+          </div>
+          <div className="flex flex-row-reverse gap-2">
+            <label htmlFor="">€100-200</label>
+            <input type="checkbox" />
+          </div>
+          <div className="flex flex-row-reverse gap-2">
+            <label htmlFor="">€200+</label>
+            <input type="checkbox" />
+          </div>
+        </div>
+        <div className="flex flex-col items-start font-sans">
+          <h3 className="font-bold text-base mb-1">Price per night</h3>
+          <div className="flex flex-row-reverse gap-2">
+            <label htmlFor="">Under €50</label>
+            <input type="checkbox" />
+          </div>
+          <div className="flex flex-row-reverse gap-2">
+            <label htmlFor="">€50-100</label>
+            <input type="checkbox" />
+          </div>
+          <div className="flex flex-row-reverse gap-2">
+            <label htmlFor="">€100-200</label>
+            <input type="checkbox" />
+          </div>
+          <div className="flex flex-row-reverse gap-2">
+            <label htmlFor="">€200+</label>
+            <input type="checkbox" />
+          </div>
+        </div>
+      </form>
+      <button></button>
+    </>
+  );
+}
+
+export default FilterButton;
--- src/pages/Venues/index.tsx
+++ src/pages/Venues/index.tsx
@@ -1,13 +1,20 @@
 import VenueSquareListing from "../../comp/VenueSquareListing";
 import GoogleMapsSearch from "../../comp/GoogleMapsSearch";
+import FilterButton from "../../comp/FilterButton";
 
 function Venues() {
   return (
-    <div className="flex flex-col gap-4">
-      <GoogleMapsSearch />
-      <ul className="grid grid-cols-1 xs:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-        <VenueSquareListing />
-      </ul>
+    <div className="flex flex-row gap-4">
+      <section className="w-full">
+        <h2 className="text-2xl font-serif font-bold mb-2">Top Listing</h2>
+        <ul className="grid grid-cols-1 xs:grid-cols-2 lg:grid-cols-3 gap-4">
+          <VenueSquareListing />
+        </ul>
+      </section>
+      <div className="flex flex-col gap-4 items-end w-full">
+        <GoogleMapsSearch />
+        <FilterButton />
+      </div>
     </div>
   );
 }
