e14d538f71aa8c6f9143e750136367492afc2d42
nicolaibye <nicbye02217@stud.noroff.no>
2025-11-20 11:34:38 +0100

    Created square venue listing

---
+++ src/comp/VenueSquareListing/index.tsx
@@ -0,0 +1,58 @@
+import { useAdventureContext } from "../../context/AdventureContext/useAdventureContext";
+
+function VenueSquareListing() {
+  const { adventureType } = useAdventureContext();
+  return (
+    <li className={`w-full shrink-0 h-auto aspect-square flex flex-col`}>
+      <div className="flex-1 object-cover relative">
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSR3p565lkyXBLlkNemqfGmUAWtVxN5XT6cQg&s"
+          alt=""
+          className="object-cover w-full h-full rounded-t-xs"
+        />
+        {adventureType === "affluent" ? (
+          <>
+            <button className="absolute bottom-2 left-2 square-venue-label">
+              Express Checkout &#x2192;
+            </button>
+          </>
+        ) : (
+          <>
+            <ul
+              className={`flex flex-row flex-wrap gap-1 absolute bottom-2 left-2 ${adventureType === "guardian" ? "block" : "hidden"} `}
+            >
+              <li className="square-venue-label w-fit">Free cancellation</li>
+              <li className="square-venue-label">
+                Total include taxes and fees
+              </li>
+            </ul>
+            <ul
+              className={`flex flex-row flex-wrap gap-1 absolute bottom-2 left-2 ${adventureType === "explorer" ? "block" : "hidden"} `}
+            >
+              <li className="square-venue-label w-fit">Fast Wi-Fi</li>
+              <li className="square-venue-label w-fit">Pool</li>
+              <li className="square-venue-label w-fit">Breakfast included</li>
+            </ul>
+          </>
+        )}
+      </div>
+      <div className="font-sans bg-hdYellow p-2 flex flex-col gap-2 rounded-b-xs">
+        <div>
+          <h3 className="font-sans font-bold leading-5">
+            Clarion Hotel The Hub
+          </h3>
+          <p>Milan, Italy</p>
+        </div>
+        <div className="flex flex-row items-center gap-2 text-sm">
+          <p className="font-bold">4.77</p>
+          <div className="bg-hdBlack w-0.5 h-0.5 rounded-full"></div>
+          <p>
+            <span className="font-bold">£500</span> for 4 nights
+          </p>
+        </div>
+      </div>
+    </li>
+  );
+}
+
+export default VenueSquareListing;
--- src/pages/Venues/index.tsx
+++ src/pages/Venues/index.tsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router";
+import VenueSquareListing from "../../comp/VenueSquareListing";
 
 function Venues() {
   return (
@@ -6,6 +7,9 @@ function Venues() {
       Venues Page
       <Link to="/venue/1">Go to Venue 1</Link>
       <Link to="/venue/2">Go to Venue 2</Link>
+      <ul className="grid grid-cols-1 xs:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        <VenueSquareListing />
+      </ul>
     </div>
   );
 }
