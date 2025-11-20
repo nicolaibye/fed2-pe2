28e3dee03764987035e1519f038cb5edf0c37761
nicolaibye <nicbye02217@stud.noroff.no>
2025-11-20 12:22:01 +0100

    Added resizing for desktop

--- src/comp/GoogleMapsSearch/index.tsx
+++ src/comp/GoogleMapsSearch/index.tsx
@@ -1,6 +1,16 @@
+import { ArrowsOutSimpleIcon } from "@phosphor-icons/react";
+
 function GoogleMapsSearch() {
+  const resizeMap = () => {
+    const map = document.querySelector("#venueMapSearch");
+    map?.classList.toggle("w-full");
+    map?.classList.toggle("w-1/2");
+  };
   return (
-    <div className="w-full h-96 rounded-xs">
+    <div
+      id="venueMapSearch"
+      className="w-full h-48 md:h-auto rounded-xs relative md:aspect-square transition-all duration-300"
+    >
       <iframe
         src="https://maps.google.com/maps?q=59.9139,10.7522&z=15&output=embed"
         frameborder="0"
@@ -10,6 +20,12 @@ function GoogleMapsSearch() {
         loading="lazy"
         referrerpolicy="no-referrer-when-downgrade"
       ></iframe>
+      <button
+        onClick={resizeMap}
+        className="absolute bottom-2 left-2 p-2 bg-hdYellow rounded-full hidden md:block"
+      >
+        <ArrowsOutSimpleIcon size={24} weight="regular" />
+      </button>
     </div>
   );
 }
