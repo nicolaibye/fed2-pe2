254745bcead3f1e012803e7e03d211ac6b2c27c3
nicolaibye <nicbye02217@stud.noroff.no>
2025-11-25 12:51:47 +0100

    Flex adjusting to map size

--- src/pages/Venues/index.tsx
+++ src/pages/Venues/index.tsx
@@ -4,14 +4,14 @@ import FilterButton from "../../comp/FilterButton";
 
 function Venues() {
   return (
-    <div className="flex flex-row gap-4">
-      <section className="w-full">
+    <div className="flex flex-col-reverse md:flex-row gap-4 w-full">
+      <section className="grow">
         <h2 className="text-2xl font-serif font-bold mb-2">Top Listing</h2>
         <ul className="grid grid-cols-1 xs:grid-cols-2 lg:grid-cols-3 gap-4">
           <VenueSquareListing />
         </ul>
       </section>
-      <div className="flex flex-col gap-4 items-end w-full">
+      <div className="flex flex-col gap-4 items-end flex-1">
         <GoogleMapsSearch />
         <FilterButton />
       </div>
