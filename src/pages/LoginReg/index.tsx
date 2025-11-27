cad9858e63e962da62ddc4210b75e815209bea50
nicolaibye <nicbye02217@stud.noroff.no>
2025-11-27 14:23:46 +0100

    Added shadows to DestinationSection

--- src/pages/LoginReg/index.tsx
+++ src/pages/LoginReg/index.tsx
@@ -1,5 +1,20 @@
 function LoginReg() {
-  return <div>LoginReg Page</div>;
+  return (
+    <div>
+      <img src="" alt="" />
+      <h1>LoginReg</h1>
+      <form action="">
+        <input type="email" placeholder="Email" />
+        <input type="password" placeholder="Password" />
+        <textarea name="" id="" cols={30} rows={10}></textarea>
+        <input type="text" />
+        <input type="text" />
+        <input type="checkbox" />
+        <button type="submit"></button>
+      </form>
+      <p>Already have an account ? </p>
+    </div>
+  );
 }
 
 export default LoginReg;
