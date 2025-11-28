6d46201f4041c456b5d2de01877fd4faf958b6af
nicolaibye <nicbye02217@stud.noroff.no>
2025-11-28 17:32:28 +0100

    Add interface for venues and basic api fetch

--- src/comp/VenueSquareListing/index.tsx
+++ src/comp/VenueSquareListing/index.tsx
@@ -1,57 +1,105 @@
+import { useEffect, useState } from "react";
 import { useAdventureContext } from "../../context/AdventureContext/useAdventureContext";
+
+const url = "https://v2.api.noroff.dev/holidaze/venues";
 
 function VenueSquareListing() {
   const { adventureType } = useAdventureContext();
+
+  const [posts, setPosts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(null);
+  const [errorMessage, setErrorMessage] = useState([]);
+
+  useEffect(() => {
+    async function getData() {
+      try {
+        setIsError(false);
+        setIsLoading(true);
+        const response = await fetch(url);
+        const data = await response.json();
+        setPosts(data);
+
+        setIsLoading(false);
+      } catch (error) {
+        setErrorMessage(error);
+        setIsLoading(false);
+        setIsError(true);
+      }
+    }
+
+    getData();
+  }, []);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    return <div>Error loading data. {errorMessage}</div>;
+  }
+
   return (
-    <li className={`w-full shrink-0 h-auto aspect-square flex flex-col`}>
-      <div className="flex-1 object-cover relative">
-        <img
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSR3p565lkyXBLlkNemqfGmUAWtVxN5XT6cQg&s"
-          alt=""
-          className="object-cover w-full h-full rounded-t-xs"
-        />
-        {adventureType === "affluent" ? (
-          <>
-            <button className="absolute bottom-2 left-2 square-venue-label">
-              Express Checkout &#x2192;
-            </button>
-          </>
-        ) : (
-          <>
-            <ul
-              className={`flex flex-row flex-wrap gap-1 absolute bottom-2 left-2 ${adventureType === "guardian" ? "block" : "hidden"} `}
-            >
-              <li className="square-venue-label w-fit">Free cancellation</li>
-              <li className="square-venue-label">
-                Total include taxes and fees
-              </li>
-            </ul>
-            <ul
-              className={`flex flex-row flex-wrap gap-1 absolute bottom-2 left-2 ${adventureType === "explorer" ? "block" : "hidden"} `}
-            >
-              <li className="square-venue-label w-fit">Fast Wi-Fi</li>
-              <li className="square-venue-label w-fit">Pool</li>
-              <li className="square-venue-label w-fit">Breakfast included</li>
-            </ul>
-          </>
-        )}
-      </div>
-      <div className="font-sans bg-hdYellow p-2 flex flex-col gap-2 rounded-b-xs">
-        <div>
-          <h3 className="font-sans font-bold leading-5">
-            Clarion Hotel The Hub
-          </h3>
-          <p>Milan, Italy</p>
-        </div>
-        <div className="flex flex-row items-center gap-2 text-sm">
-          <p className="font-bold">4.77</p>
-          <div className="bg-hdBlack w-0.5 h-0.5 rounded-full"></div>
-          <p>
-            <span className="font-bold">£500</span> for 4 nights
-          </p>
-        </div>
-      </div>
-    </li>
+    <>
+      {posts.map((post) => (
+        <li
+          key={post.id}
+          className={`w-full shrink-0 h-auto aspect-square flex flex-col`}
+        >
+          <div className="flex-1 object-cover relative">
+            <img
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSR3p565lkyXBLlkNemqfGmUAWtVxN5XT6cQg&s"
+              alt=""
+              className="object-cover w-full h-full rounded-t-xs"
+            />
+            {adventureType === "affluent" ? (
+              <>
+                <button className="absolute bottom-2 left-2 square-venue-label">
+                  Express Checkout &#x2192;
+                </button>
+              </>
+            ) : (
+              <>
+                <ul
+                  className={`flex flex-row flex-wrap gap-1 absolute bottom-2 left-2 ${adventureType === "guardian" ? "block" : "hidden"} `}
+                >
+                  <li className="square-venue-label w-fit">
+                    Free cancellation
+                  </li>
+                  <li className="square-venue-label">
+                    Total include taxes and fees
+                  </li>
+                </ul>
+                <ul
+                  className={`flex flex-row flex-wrap gap-1 absolute bottom-2 left-2 ${adventureType === "explorer" ? "block" : "hidden"} `}
+                >
+                  <li className="square-venue-label w-fit">Fast Wi-Fi</li>
+                  <li className="square-venue-label w-fit">Pool</li>
+                  <li className="square-venue-label w-fit">
+                    Breakfast included
+                  </li>
+                </ul>
+              </>
+            )}
+          </div>
+          <div className="font-sans bg-hdYellow p-2 flex flex-col gap-2 rounded-b-xs">
+            <div>
+              <h3 className="font-sans font-bold leading-5">
+                Clarion Hotel The Hub
+              </h3>
+              <p>Milan, Italy</p>
+            </div>
+            <div className="flex flex-row items-center gap-2 text-sm">
+              <p className="font-bold">4.77</p>
+              <div className="bg-hdBlack w-0.5 h-0.5 rounded-full"></div>
+              <p>
+                <span className="font-bold">£500</span> for 4 nights
+              </p>
+            </div>
+          </div>
+        </li>
+      ))}
+    </>
   );
 }
 
---
+++ src/types/venue.ts
@@ -0,0 +1,61 @@
+export interface ApiResponse {
+  data: Venue[];
+  meta: Record<string, unknown>;
+}
+
+export interface Venue {
+  id: string;
+  name: string;
+  description: string;
+  media: MediaItem[];
+  price: number;
+  maxGuests: number;
+  rating: number;
+  created: string;
+  updated: string;
+  meta: VenueMeta;
+  location: VenueLocation;
+  owner: VenueOwner;
+  bookings: Booking[];
+  _count: {
+    bookings: number;
+  };
+}
+
+export interface MediaItem {
+  url: string;
+  alt: string;
+}
+
+export interface VenueMeta {
+  wifi: boolean;
+  parking: boolean;
+  breakfast: boolean;
+  pets: boolean;
+}
+
+export interface VenueLocation {
+  address: string;
+  city: string;
+  zip: string;
+  country: string;
+  continent: string;
+  lat: number;
+  lng: number;
+}
+
+export interface VenueOwner {
+  name: string;
+  email: string;
+  bio: string;
+  avatar: MediaItem;
+  banner: MediaItem;
+}
+
+export interface Booking {
+  name: string;
+  email: string;
+  bio: string;
+  avatar: MediaItem;
+  banner: MediaItem;
+}
