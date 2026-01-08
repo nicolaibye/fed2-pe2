c8a3b1064be5a66a4d4a0cf1bb046d8ed1518171
nicolaibye <nicbye02217@stud.noroff.no>
2026-01-08 21:47:03 +0100

    Adjusted home cards and page title

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -6,15 +6,7 @@ function Home() {
   return (
     <div className="flex gap-5 flex-col">
       <h1 className="sr-only">Holidaze Venue homepage</h1>
-      <div className="flex flex-col md:flex-row gap-5">
-        <NewsCard />
-        <div className="hidden md:block">
-          <NewsCard />
-        </div>
-        <div className="hidden md:block">
-          <NewsCard />
-        </div>
-      </div>
+      <NewsCard />
       <section>
         <h2 className="text-2xl font-serif font-bold mb-2">Let us inspire</h2>
         <ul className="flex flex-row gap-2.5 md:gap-0 overflow-x-scroll md:overflow-x-auto justify-start md:justify-center rounded-xs no-scrollbar">
